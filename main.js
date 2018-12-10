926f3169506202cfeee31817c6d933ab2968c588
LordGoblin <maxime78600@gmail.com>
2018-12-10 19:00:50 +0100

    lol

---
+++ main.js
@@ -0,0 +1,13 @@
+
+setTimeout(function()
+{
+	var txt;
+	if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) 
+	{
+		window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+	}
+	else
+	{
+		txt = "pressed cancel!";
+	}
+},2000);
